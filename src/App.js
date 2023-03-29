b079a19d7314fd29d3aabcf3e177bdd2bfb026f1
Flasky06 <bonnienjuguna106@gmail.com>
2023-03-29 13:20:58 +0300

    responsiveness

--- src/App.js
+++ src/App.js
@@ -1,37 +1,18 @@
-import Categories from "./Components/Categories";
-import Pages from "./Pages/Pages";
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HeadlineCards from './components/HeadlineCards'
+import Food from './components/Food'
+import Category from './components/Category'
 
-import Search from "./Components/Search";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
-import Cuisine from "./Pages/Cuisine";
-import Searched from "./Pages/Searched";
-import Recipe from "./Pages/Recipe";
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        {/* <Pages /> */}
-        {/* <Search /> */}
-        <Categories />
-        <Routes>
-          <Route
-            children={({ location }) => {
-              return !["/recipe"].includes(location.pathname) ? (
-                <>
-                  <Search />
-                </>
-              ) : (
-                <Search />
-              );
-            }}
-          ></Route>
-          <Route path="/recipe/:name" element={<Recipe />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/cuisine/:type" element={<Cuisine />} />
-          <Route path="/searched/:search" element={<Searched />} />
-        </Routes>
-      </BrowserRouter>
+    <div>
+        <Navbar />
+        <Hero />
+        <HeadlineCards />
+        <Food />
+        <Category />
     </div>
   );
 }
